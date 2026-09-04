8cd74aa9274de703177fa5eeee4c29e6b028d5da
fabyanism <fabdeyy@gmail.com>
2026-09-04 14:48:15 +0700

    Praktikum1

---
+++ pekan1/program1_2611531004.py
@@ -0,0 +1,12 @@
+# Program pertama
+"""
+Komentar
+dua baris atau lebih
+pertemuan 1 praktikum alpro
+"""
+print("Hello World")
+print()
+print("Departemen Informatika")
+print("UNiversitas Andalas")
+print("/ \\ // \\\\ /// \\\\\\")
+print("program ini" + " dibuat oleh a\\n " + "Fabian Alhadi Zalinov dan 2611531004.")
