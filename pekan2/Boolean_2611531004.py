e30db24ddc0e8bb5a92537148b4243ed0f5e1fe4
fabyanism <fabdeyy@gmail.com>
2026-09-10 23:19:13 +0700

    Laprak 1

---
+++ pekan2/Boolean_2611531004.py
@@ -0,0 +1,17 @@
+# Buat file dengan nama Boolean_2611531004.py
+# Buat variabel ditambah 4 digit nim terakhir contoh: nilai_1004
+# Deklarasi variabel dengan tipe data Boolean
+is_lulus = True
+is_cumlaude = True
+
+# Menggunakan Boolean
+nilai = 85
+batas_lulus = 75
+# Menentukan nilai Boolean dari kondisi
+status_kelulusan = nilai >= batas_lulus  # Hasilnya akan True
+
+print("=== Check Kelulusan ===")
+print("Nilai:", nilai)
+print("Apakah Lulus?:", status_kelulusan)
+if is_lulus and is_cumlaude:
+    print("Selamat, Anda lulus dengan predikat Cum laude!")
